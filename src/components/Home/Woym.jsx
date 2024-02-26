391a4957ba3f289e19911aa97adfa7a831ddb04a
spartan-readytocode <7007966033abc@gmail.com>
2024-02-26 09:45:07 +0530

    feat : search box

--- src/components/Home/Woym.jsx
+++ src/components/Home/Woym.jsx
@@ -26,7 +26,7 @@ const responsive = {
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 4,
+    items: 3,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { CiSearch } from "react-icons/ci";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="flex items-center align-middle justify-center p-6">
+      <div className="flex  align-middle items-center border-2 border-gray-400">
+        <form
+          action="
+    "
+        >
+          <input
+            type="text"
+            name=""
+            id=""
+            className="md:w-[445px] lg:w-[556px] h-11 border border-none pointer-events-auto hover:border-orange-300  "
+            placeholder=" Enter the restaurant you want to search"
+          />
+        </form>
+        <button>
+          {" "}
+          <CiSearch size={25} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
